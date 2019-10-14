ee962cb971acc537715f61846027b4f8b2a89894
Yair <yair_918@hotmail.com>
2019-10-13 19:19:48 -0500

    Finished the project

---
+++ src/components/CarouselItem.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../assets/styles/components/CarouselItem.scss';
+import playIcon from '../assets/styles/static/play-icon.png';
+import plusIcon from '../assets/styles/static/plus-icon.png';
+
+const CarouselItem = ({ cover, title, year, contentRaiting, duration }) => {
+    return (
+        <div className="carousel-item">
+            <img className="carousel-item__img" src={cover} alt={title} />
+            <div className="carousel-item__details">
+                <div>
+                    <img className="carousel-item__details--img" src={playIcon} alt="Play Icon" />
+                    <img className="carousel-item__details--img" src={plusIcon} alt="Plus Icon" />
+                </div>
+                <p className="carousel-item__details--title">{title}</p>
+                <p className="carousel-item__details--subtitle">2019 16+ 114 minutos
+                    {`${year} ${contentRaiting} ${duration}`}
+                </p>
+            </div>
+        </div>
+    );
+}
+
+CarouselItem.propTypes = {
+    cover: PropTypes.string,
+    title: PropTypes.string,
+    year: PropTypes.number,
+    contentRaiting: PropTypes.string,
+    duration: PropTypes.number
+}
+
+export default CarouselItem;
